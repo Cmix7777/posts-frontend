39ea2cc4828b1c6e7e0b3f6c176a3f3bcccf6498
Cmix7777 <cmix771@gmail.com>
2025-06-03 17:46:33 +0400

    Eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['@/assets/scss/main.scss'],
-  
+  app:{
+    head:{
+      title:'Управляй своими постами - PostVibe',
+      meta:[
+        { name:'description', content:'Создавай и читай посты на лучшем сайта этого рода - PostVibe, место где создаются мысли и идеи'
+        }
+      ]
+    }
+  },
   vite:{
     css:{
       preprocessorOptions:{
